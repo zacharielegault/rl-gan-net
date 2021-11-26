73cbf152921ff0c739625f7736f2e27a53327133
Z <z>
2021-11-25 23:21:08 -0500

    Adjust whitespace to follow PEP8

--- RL/DDPG.py
+++ RL/DDPG.py
@@ -7,70 +7,63 @@ import utils
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# by using  and tuning DDPG from Paper: https://arxiv.org/abs/1509.02971
+# by using and tuning DDPG from Paper: https://arxiv.org/abs/1509.02971
 
 
 class Actor(nn.Module):
 	def __init__(self, state_dim, action_dim, max_action):
-		super(Actor, self).__init__()
+		super().__init__()
 
 		self.l1 = nn.Linear(state_dim, 400)
 		self.l2 = nn.Linear(400, 300)
-        self.l2_add = nn.Linear(400, 300)
+		self.l2_add = nn.Linear(400, 300)
 		self.l3 = nn.Linear(300, action_dim)
 		
 		self.max_action = max_action
 
-	
 	def forward(self, x):
 		x = F.relu(self.l1(x))
 		x = F.relu(self.l2(x))
-        x = F.relu(self.l2_add(x))
+		x = F.relu(self.l2_add(x))
 		x = self.max_action * torch.tanh(self.l3(x)) 
 		return x 
 
 
 class Critic(nn.Module):
 	def __init__(self, state_dim, action_dim):
-		super(Critic, self).__init__()
+		super().__init__()
 
 		self.l1 = nn.Linear(state_dim + action_dim, 400)
 		self.l2 = nn.Linear(400, 300)
-        self.l3_add = nn.Linear(300, 300)
+		self.l3_add = nn.Linear(300, 300)
 		self.l3 = nn.Linear(300, 1)
-
 
 	def forward(self, x, u):
 		x = F.relu(self.l1(torch.cat([x, u], 1)))
 		x = F.relu(self.l2(x))
-        x = self.l3_add(x)
+		x = self.l3_add(x)
 		x = self.l3(x)
 		return x 
 
 
 class DDPG(object):
 	def __init__(self, state_dim, action_dim, max_action):
-        
 		self.actor = Actor(state_dim, action_dim, max_action).to(device)
 		self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
 		self.actor_target.load_state_dict(self.actor.state_dict())
-		self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),lr=1e-3)
+		self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=1e-3)
 
 		self.critic = Critic(state_dim, action_dim).to(device)
 		self.critic_target = Critic(state_dim, action_dim).to(device)
 		self.critic_target.load_state_dict(self.critic.state_dict())
-		self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),W_decay=1e-2)		
-
+		self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), weight_decay=1e-2)
 
 	def select_action(self, state):
 		state = torch.FloatTensor(state.reshape(1, -1)).to(device)
 		return self.actor(state).cpu().data.numpy().flatten()
 
-
 	def train(self, replay_buffer, iterations, batch_size=32, discount=0.99, tau=0.001):
-
 		for it in range(iterations):
-
 			# Sample replay buffer 
 			x, y, u, r, d = replay_buffer.sample(batch_size)
 			state = torch.FloatTensor(x).to(device)
@@ -109,11 +102,9 @@ class DDPG(object):
 			for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
 				target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
 
-
 	def save(self, filename, directory):
 		torch.save(self.actor.state_dict(), '%s/%s_actor.pth' % (directory, filename))
 		torch.save(self.critic.state_dict(), '%s/%s_critic.pth' % (directory, filename))
-
 
 	def load(self, filename, directory):
 		self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, filename)))
