26d66696ebd12a65df4a82c5a7ef9953c2a46393
Z <z>
2021-11-29 21:52:01 -0500

    Dtype selection when loading point clouds

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import open3d as o3d
 
 
-def read_pointcloud(ply_file_path: str, subsample: Optional[int] = None) -> np.ndarray:
+def read_pointcloud(ply_file_path: str, subsample: Optional[int] = None, dtype: type = np.float32) -> np.ndarray:
     """
     Example usage to gather and subsample all PLY files in a directory:
     >>> import os
@@ -11,7 +11,7 @@ def read_pointcloud(ply_file_path: str, subsample: Optional[int] = None) -> np.n
     >>> crowns = np.asarray([read_pointcloud(os.path.join(directory, f), 2048) for f in os.listdir(directory)])
     """
     cloud = o3d.io.read_point_cloud(ply_file_path)
-    cloud = np.asarray(cloud.points)  # np.ndarray with shape (n, 3)
+    cloud = np.asarray(cloud.points, dtype=dtype)  # np.ndarray with shape (n, 3)
 
     if subsample is not None:
         indices = np.random.choice(len(cloud), subsample, replace=False)
