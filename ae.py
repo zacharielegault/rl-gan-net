ee3513bf22a5fddd8daaf726611e4890ae7e1714
Zacharie Legault <zacharie@hudsonlegault.net>
2021-12-09 17:57:03 -0500

    Fix mistakes to actually use config file

--- ae.py
+++ ae.py
@@ -25,6 +25,7 @@ class AutoEncoder(pl.LightningModule):
         self.decoder = Decoder(decoder_dimensions, num_points)
         self.split = split
         self.batch_size = batch_size
+        self.num_points = num_points
 
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """Expects tensor of shape (batch_size, 3, num_points).
@@ -42,21 +43,23 @@ class AutoEncoder(pl.LightningModule):
         return loss
 
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
-        loss = self.training_step(batch, batch_idx)
+        input_clouds, target_clouds = batch
+        predicted_clouds = self.decoder(self.encoder(input_clouds))
+        loss = chamfer_loss(predicted_clouds, target_clouds)
         self.log("loss/val", loss, on_step=False, on_epoch=True)
         return loss
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> torch.optim.Optimizer:
         return torch.optim.Adam(self.parameters(), lr=0.02)
 
-    def train_dataloader(self):
+    def train_dataloader(self) -> DataLoader:
         num_workers = os.cpu_count()
         train_dataset = DentalArchesDataset(
             csv_filepath=f"data/kfold_split/split_{self.split}_train.csv",
             context_directory="data/preprocessed_partitions",
             opposing_directory="data/opposing_partitions",
             crown_directory="data/crowns",
-            num_points=2048,
+            num_points=self.num_points,
         )
 
         return DataLoader(
@@ -66,22 +69,23 @@ class AutoEncoder(pl.LightningModule):
             num_workers=num_workers,
             pin_memory=True,
             persistent_workers=num_workers > 0,
+            drop_last=True,
         )
 
-    def val_dataloader(self):
+    def val_dataloader(self) -> DataLoader:
         num_workers = os.cpu_count()
         val_dataset = DentalArchesDataset(
                 csv_filepath=f"data/kfold_split/split_{self.split}_val.csv",
                 context_directory="data/preprocessed_partitions",
                 opposing_directory="data/opposing_partitions",
                 crown_directory="data/crowns",
-                num_points=2048,
+                num_points=self.num_points,
         )
 
         return DataLoader(
             val_dataset,
             shuffle=False,
-            batch_size=self.batch_size,
+            batch_size=1,
             num_workers=num_workers,
             pin_memory=True,
             persistent_workers=num_workers > 0,
@@ -162,7 +166,12 @@ def main(args: argparse.Namespace):
     assert config_is_valid(config)
 
     # Define model
-    model = AutoEncoder([3, 64, 128, 256, 128], [128, 256, 256, 3], 2048, config.split)
+    model = AutoEncoder(
+        encoder_dimensions=config.autoencoder.encoder_dimensions,
+        decoder_dimensions=config.autoencoder.decoder_dimensions,
+        num_points=config.num_points,
+        split=config.split
+    )
 
     # Train model
     checkpoint_callback = ModelCheckpoint(monitor="loss/val", verbose=True)
