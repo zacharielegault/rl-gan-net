b2c3ae7c4d81598ff8af3a4178ba7f9ded188138
Zacharie Legault <zacharie@hudsonlegault.net>
2021-12-09 18:12:21 -0500

    Shell script to train everything from scratch

---
+++ train_all.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+start_time=$(date +%Y_%m_%d_%H_%M_%S)
+config_path=config_${start_time}.yaml
+
+cp default_config.yaml "${config_path}"
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate rl-gan-net
+python ae.py --config "${config_path}" --save_checkpoint
+python gan.py --config "${config_path}" --save_checkpoint
+python rl_gan_net.py --config "${config_path}"
